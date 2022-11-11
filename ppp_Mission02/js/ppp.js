8614797e4e7843550621469fb71cbecfd16a54cc
kin698 <116795076+kin698@users.noreply.github.com>
2022-11-11 20:04:17 +0900

    ppp_Mission2

---
+++ ppp_Mission02/js/ppp.js
@@ -0,0 +1,29 @@
+function popupZipSearch(){
+    new daum.Postcode({
+    oncomplete: function(data) {
+    
+      var fullAddr = '';
+      var extraAddr = '';
+      if (data.userSelectedType === 'R') {
+          fullAddr = data.roadAddress;
+      } else { 
+          fullAddr = data.jibunAddress;
+      }
+     
+      if(data.userSelectedType === 'R'){
+          if(data.bname !== ''){
+              extraAddr += data.bname;
+          }
+          if(data.buildingName !== ''){
+              extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+          }
+          fullAddr += (extraAddr !== '' ? ' ('+ extraAddr +')' : '');
+      }
+    
+      document.getElementById('me_org_addr2').value = data.zonecode;
+      document.getElementById("me_org_addr1").value = fullAddr;
+      document.getElementById("me_org_addr2").focus();
+    }
+     
+    }).open();
+    }
